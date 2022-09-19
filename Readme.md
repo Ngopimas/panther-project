00eeeecdd7ed80a966589d0a79f13232540e3a4e
Romain Coupey <schaft115@hotmail.com>
2022-09-19 21:54:06 +0200

    add base layout for the Panther Project

--- Readme.md
+++ Readme.md
@@ -1,37 +1,54 @@
-# WebGi starter project
+# Based on WebGi starter project
+
 A template for a vanilla(no ui-framework) project with webgi engine in typescript using parcel bundler.
 
 About webgi: [https://webgi.xyz/](https://webgi.xyz/)
 
 ## Running
+
 First install the dependencies:
+
 ```bash
 npm install
 ```
 
 To run the project in development mode:
+
 ```bash
 npm start
 ```
+
 Then navigate to [http://localhost:1234/index.html](http://localhost:1234/index.html) in a web browser to see the default scene in a viewer.
 
 The assets are stored in the `assets` directory.
 
 To build the project for production:
+
 ```bash
 npm run build
 ```
 
 ## Updates
+
 Check the [webgi manual](https://webgi.xyz/docs/manual/#sdk-links) for the latest version.
 To use the different version:
-* Update the version number in `package.json` file for both `webgi` and `@types/webgi`.
-* Run `npm install` to update the dependencies.
-* Delete `.cache` folder created by parcel bundler: `rm -rf .cache`
-* Run `npm start` or `npm run build` to run or build the project.
+
+- Update the version number in `package.json` file for both `webgi` and `@types/webgi`.
+- Run `npm install` to update the dependencies.
+- Delete `.cache` folder created by parcel bundler: `rm -rf .cache`
+- Run `npm start` or `npm run build` to run or build the project.
 
 ## Documentation
+
 For the latest version and documentation: [WebGi Docs](https://webgi.xyz/docs/).
 
-## License 
+## License
+
 For license and terms of use, see the [SDK License](https://webgi.xyz/docs/license).
+
+## Assets
+
+3D Model from AR Quick Look USDZ
+https://sketchfab.com/3d-models/cartier-panthere-48f03527a21243bcace6e029a48179fa
+
+## Project inspired by Anderson Mancini
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,152 @@
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-
-    addBasePlugins,
-    ITexture,
-
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
 } from "webgi";
 import "./styles.css";
 
-async function setupViewer(){
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
-        useRgbm: true,
+gsap.registerPlugin(ScrollTrigger);
+
+async function setupViewer() {
+  const viewer = new ViewerApp({
+    canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+    useRgbm: false,
+    isAntialiased: true,
+  });
+
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+  const camera = viewer.scene.activeCamera;
+  const position = camera.position;
+  const target = camera.target;
+
+  // Add plugins individually.
+  await viewer.addPlugin(GBufferPlugin);
+  await viewer.addPlugin(new ProgressivePlugin(32));
+  await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm));
+  await viewer.addPlugin(SSRPlugin);
+  await viewer.addPlugin(SSAOPlugin);
+  await viewer.addPlugin(BloomPlugin);
+
+  viewer.renderer.refreshPipeline();
+
+  await manager.addFromPath("./assets/scene.glb");
+
+  function setupScrollanimation() {
+    const tl = gsap.timeline();
+
+    // FIRST SECTION
+
+    tl.to(position, {
+      x: -3.83,
+      y: -2.32,
+      z: -5.71,
+      scrollTrigger: {
+        trigger: ".second",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+      onUpdate,
     })
 
-    // Add some plugins
-    const manager = await viewer.addPlugin(AssetManagerPlugin)
+      .to(".section--one--container", {
+        xPercent: "-150",
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".second",
+          start: "top bottom",
+          end: "top 80%",
+          scrub: 1,
+          immediateRender: false,
+        },
+      })
+      .to(target, {
+        x: -0.72,
+        y: -0.32,
+        z: -0.74,
+        scrollTrigger: {
+          trigger: ".second",
+          start: "top bottom",
+          end: "top top",
+          scrub: true,
+          immediateRender: false,
+        },
+      })
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+      // LAST SECTION
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+      .to(position, {
+        x: 4,
+        y: 2,
+        z: 7,
+        scrollTrigger: {
+          trigger: ".third",
+          start: "top bottom",
+          end: "top top",
+          scrub: true,
+          immediateRender: false,
+        },
+        onUpdate,
+      })
 
-    viewer.renderer.refreshPipeline()
+      .to(target, {
+        x: -1,
+        y: -9.5,
+        z: -0.5,
+        scrollTrigger: {
+          trigger: ".third",
+          start: "top bottom",
+          end: "top top",
+          scrub: true,
+          immediateRender: false,
+        },
+      });
+  }
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+  setupScrollanimation();
 
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
+  // WEBGI UPDATE
+  let needsUpdate = true;
 
+  function onUpdate() {
+    needsUpdate = true;
+    viewer.renderer.resetShadows();
+    viewer.setDirty();
+  }
 
+  viewer.addEventListener("preFrame", () => {
+    if (needsUpdate) {
+      camera.positionUpdated(true);
+      camera.targetUpdated(true);
+      needsUpdate = false;
+    }
+  });
+
+  // KNOW MORE EVENT
+  document.querySelector(".button--hero")?.addEventListener("click", () => {
+    const element = document.querySelector(".second");
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+
+  // SCROLL TO TOP
+  document.querySelectorAll(".button--footer")?.forEach((item) => {
+    item.addEventListener("click", () => {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    });
+  });
 }
 
-setupViewer()
+setupViewer();
